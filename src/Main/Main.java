76dd5327c597be66be6cb0ad42bf44db5cf51ddd
mberr59 <mberr59@wgu.edu>
2022-02-11 14:50:59 -0500

    Initial Commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View/LoginScreen.fxml"));
         primaryStage.setTitle("Login Screen");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
