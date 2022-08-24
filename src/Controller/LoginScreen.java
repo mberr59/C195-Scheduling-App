c6f8d2af22916ca29c6118202b39723f648bb438
mberr59 <mberr59@wgu.edu>
2022-08-23 21:37:37 -0700

    Login validation complete. Create reports and log next.

--- src/Controller/LoginScreen.java
+++ src/Controller/LoginScreen.java
@@ -1,11 +1,19 @@
 package Controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.ZoneId;
+
+import Helper.DBConnection;
+import Helper.QueryExecutions;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -27,23 +35,67 @@ public class LoginScreen implements Initializable {
     private final ZoneId z = ZoneId.systemDefault();
 
     public void userNameTextHandler() {
+        try {
+            Connection conn = DBConnection.getConn();
+            boolean nameFound = false;
+            String providedUsername = userNameText.getText();
+            PreparedStatement usernameStatement = conn.prepareStatement(QueryExecutions.getUsernames());
+            ResultSet usernameRS = usernameStatement.executeQuery();
+            while (usernameRS.next()) {
+                String result = usernameRS.getString("User_Name");
+                if (providedUsername.equals(result)) {
+                    nameFound = true;
+                    passwordTextHandler(providedUsername);
+                }
+            }
+            if (!nameFound) {
+                Alert usernameAlert = new Alert(Alert.AlertType.ERROR);
+                usernameAlert.setTitle("Incorrect Username");
+                usernameAlert.setContentText("Username provided not found.");
+                usernameAlert.showAndWait();
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
-    public void passwordTextHandler() {
+    public void passwordTextHandler(String username) {
+        try {
+            Connection conn = DBConnection.getConn();
+            boolean passFound = false;
+            String providedPassword = passwordText.getText();
+            PreparedStatement passwordStatement = conn.prepareStatement(QueryExecutions.getPassword());
+            passwordStatement.setString(1, username);
+            ResultSet passwordRS = passwordStatement.executeQuery();
+            while (passwordRS.next()) {
+                String result = passwordRS.getString("Password");
+                if (providedPassword.equals(result)) {
+                    passFound = true;
+                    Parent root;
+                    try {
+                        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/AppointmentScreen.fxml")));
+                        Stage appStage = new Stage();
+                        appStage.setTitle("Appointment Screen");
+                        appStage.setScene(new Scene(root));
+                        appStage.show();
+                    } catch (IOException ioe){
+                        ioe.printStackTrace();
+                    }
+                }
+            }
+            if (!passFound) {
+                Alert passwordAlert = new Alert(Alert.AlertType.ERROR);
+                passwordAlert.setTitle("Incorrect Password");
+                passwordAlert.setContentText("Password provided not found.");
+                passwordAlert.showAndWait();
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     public void loginButtonHandler() {
-        Parent root;
-        try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/AppointmentScreen.fxml")));
-            Stage appStage = new Stage();
-            appStage.setTitle("Appointment Screen");
-            appStage.setScene(new Scene(root));
-            appStage.show();
-        } catch (IOException ioe){
-            ioe.printStackTrace();
-        }
-
+        userNameTextHandler();
     }
 
     @Override
